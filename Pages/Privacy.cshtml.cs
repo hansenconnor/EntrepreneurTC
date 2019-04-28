53c4198b2226423534d6b3395e765e5d45b20306
hansenconnor <connorhnsn5596@gmail.com>
2019-04-27 22:42:17 -0400

    first commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace entrepreneur_tc_auth.Pages
+{
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
