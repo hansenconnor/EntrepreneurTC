0e071b15f454c2ec60dee765e51ce9d8471c6cab
hansenconnor <connorhnsn5596@gmail.com>
2019-05-02 11:33:25 -0400

    added styles to homepage

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using entrepreneur_tc_auth.Models;
+using Microsoft.EntityFrameworkCore;
+using entrepreneur_tc_auth.Data;
+using Microsoft.AspNetCore.Authorization;
 namespace entrepreneur_tc_auth.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly entrepreneur_tc_auth.Data.ApplicationDbContext _context;
+        public IndexModel(entrepreneur_tc_auth.Data.ApplicationDbContext context)
         {
+            _context = context;
+        }
 
+        public IList<Post> Posts { get; set; }
+        public async Task OnGetAsync()
+        {
+            // IQueryable<Post> studentIQ = from s in _context.Post
+                                            // select s;
+            Posts = await _context.Post.AsNoTracking().ToListAsync();
         }
     }
 }
