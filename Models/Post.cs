ecafa7acdf8f200c0b657da578717b44fe806c95
hansenconnor <connorhnsn5596@gmail.com>
2019-05-02 19:42:52 -0400

    adjusted auto increment

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace entrepreneur_tc_auth.Models
 {
     public class Post
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public string Title { get; set; }
 
