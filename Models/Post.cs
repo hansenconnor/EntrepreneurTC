ab110af12154347091f28f5c0a16567e2673a400
hansenconnor <connorhnsn5596@gmail.com>
2019-05-02 19:47:23 -0400

    ok this should work

--- Models/Post.cs
+++ Models/Post.cs
@@ -6,7 +6,7 @@ namespace entrepreneur_tc_auth.Models
 {
     public class Post
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
 
