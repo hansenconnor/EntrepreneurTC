b002dd811dece23a782e33d30a17ff7e2768516f
hansenconnor <connorhnsn5596@gmail.com>
2019-05-02 20:31:33 -0400

    adsf

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,15 +7,15 @@ using entrepreneur_tc_auth.Models;
 
 namespace entrepreneur_tc_auth.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(modelBuilder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
